a3b4e20f687361499df5ed7cf3d8681c3f57e78e
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-09-27 20:54:39 +0200

    Update 00-run-chroot.sh

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -22,7 +22,7 @@ function install_radxa-ubuntu_packages {
 function install_radxa-debian_packages {
     PLATFORM_PACKAGES_HOLD="8852be-dkms linux-image-5.10.110-6-rockchip linux-image-5.10.110-11-rockchip linux-image-rock-5a"
     #Do not remove the GUI (for Arducam) PLATFORM_PACKAGES_REMOVE="dkms xorg sddm plymouth plasma-desktop kde*"
-    PLATFORM_PACKAGES="rockchip-iq-openhd librga2=2.2.0-1 linux-image-5.10.160-16-rk356x linux-headers-5.10.160-16-rk356x linux-libc-dev-5.10.160-16-rk356x procps camera-engine-rkaiq"
+    PLATFORM_PACKAGES="rockchip-iq-openhd librga2=2.2.0-1 linux-image-5.10.160-17-rk356x linux-headers-5.10.160-17-rk356x linux-libc-dev-5.10.160-17-rk356x procps camera-engine-rkaiq"
 }
 # Ubuntu-x86-specific code
 function install_ubuntu_x86_packages {
